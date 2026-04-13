619798b3c874d9dcdd4aac8ebb9b72aa400a0368
PostSharpBot <hello+agent@postsharp.net>
2026-04-13 17:17:02 +0200

    Split fabric into separate .Fabric.cs file (#260)

--- code/Metalama.Documentation.SampleCode.AspectFramework/AdvisingTypeFabricDiagnostics.cs
+++ code/Metalama.Documentation.SampleCode.AspectFramework/AdvisingTypeFabricDiagnostics.cs
@@ -1,12 +1,5 @@
 // This is public domain Metalama sample code.
 
-using Metalama.Framework.Advising;
-using Metalama.Framework.Aspects;
-using Metalama.Framework.Code;
-using Metalama.Framework.Diagnostics;
-using Metalama.Framework.Fabrics;
-using System.Linq;
-
 namespace Doc.AdvisingTypeFabricDiagnostics;
 
 public partial class MyClass
@@ -14,31 +7,4 @@ public partial class MyClass
     public string? Name { get; set; }
 
     public override string ToString() => "MyClass";
-
-    private class Fabric : TypeFabric
-    {
-        private static readonly DiagnosticDefinition<INamedType> _warning = new(
-            "MY001",
-            Severity.Warning,
-            "The type '{0}' should have a 'Name' property." );
-
-        [Template]
-        public string ToStringTemplate()
-        {
-            return $"{meta.Target.Type.Name} (advised by fabric)";
-        }
-
-        public override void AmendType( ITypeAmender amender )
-        {
-            // Report a warning if the type does not have a 'Name' property.
-            if ( !amender.Type.Properties.OfName( "Name" ).Any() )
-            {
-                amender.Diagnostics.Report( _warning.WithArguments( amender.Type ) );
-            }
-
-            // Override ToString directly on the amender using the With method.
-            amender.With( amender.Type.Methods.OfName( "ToString" ).Single() )
-                .Override( nameof(this.ToStringTemplate) );
-        }
-    }
 }
--- code/Metalama.Documentation.SampleCode.AspectFramework/AdvisingTypeFabricDiagnostics.t.cs
+++ code/Metalama.Documentation.SampleCode.AspectFramework/AdvisingTypeFabricDiagnostics.t.cs
@@ -1,9 +1,3 @@
-using Metalama.Framework.Advising;
-using Metalama.Framework.Aspects;
-using Metalama.Framework.Code;
-using Metalama.Framework.Diagnostics;
-using Metalama.Framework.Fabrics;
-using System.Linq;
 namespace Doc.AdvisingTypeFabricDiagnostics;
 #pragma warning disable CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
 public partial class MyClass
@@ -13,13 +7,4 @@ public partial class MyClass
   {
     return "MyClass (advised by fabric)";
   }
-#pragma warning disable CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
-  private class Fabric : TypeFabric
-  {
-    private static readonly DiagnosticDefinition<INamedType> _warning = new("MY001", Severity.Warning, "The type '{0}' should have a 'Name' property.");
-    [Template]
-    public string ToStringTemplate() => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
-    public override void AmendType(ITypeAmender amender) => throw new System.NotSupportedException("Compile-time-only code cannot be called at run-time.");
-  }
-#pragma warning restore CS0067, CS8618, CS0162, CS0169, CS0414, CA1822, CA1823, IDE0051, IDE0052
 }
